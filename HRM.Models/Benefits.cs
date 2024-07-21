98c178cb9eada2ef2066da1797c26e070dd5e75c
Akai <nguyensieu12112002@gmail.com>
2024-07-21 22:33:09 +0700

    Create Benefits.cs

---
+++ HRM.Models/Benefits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Benefits
+    {
+        [Key]
+        public int Id {  get; set; }
+        public string BenefitsName { get; set; } = string.Empty;
+        public string BenefitsDescription { get; set; } = string.Empty;
+        public string TypeBenefits {  get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+    }
+}
