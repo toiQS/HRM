8194a62d5782716460f6d0b5eca1b0d30711b523
Akai <nguyensieu12112002@gmail.com>
2024-07-21 22:32:53 +0700

    Create Salary.cs

---
+++ HRM.Models/Salary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Salary
+    {
+        [Key]
+        public int Id { get; set; }
+        public float BasicSalary {  get; set; }
+        public float Bonus {  get; set; }
+        public float Deductions { get; set; } 
+        public float NetSalary { get; set; }
+        public DateTime PaymentDate { get; set; }   
+    }
+}
