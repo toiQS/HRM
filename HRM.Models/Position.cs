2cbb323b11519051010a6879fe24fa70f0caf9fa
Akai <nguyensieu12112002@gmail.com>
2024-07-21 22:32:50 +0700

    Create Position.cs

---
+++ HRM.Models/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Position
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PositonName { get; set; } = string.Empty;
+        public string PositonDescription { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(Benefits))]        
+        
+        public int BenefitsId { get; set; }
+        public virtual Benefits Benefits { get; set; }
+    }
+}
