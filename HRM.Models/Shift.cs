9faee042fa1026adbf4769f601aef237dd1a480b
Akai <nguyensieu12112002@gmail.com>
2024-07-21 22:32:55 +0700

    Create Shift.cs

---
+++ HRM.Models/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Shift
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public List<string> DayOfApplication { get; set; } = new List<string>();
+        public TimeOnly TotalHouse {  get; set; }
+    }
+}
