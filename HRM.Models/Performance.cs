0c480756391aca05a2221031a43db331ac57573a
Akai <nguyensieu12112002@gmail.com>
2024-07-21 22:32:59 +0700

    Create Performance.cs

---
+++ HRM.Models/Performance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Performance
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime EvaluationDate {  get; set; }
+        public int Score { get; set; }
+        public string Comment { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        
+        [ForeignKey(nameof(Traning))]
+        public int TraningId { get; set; }
+        public virtual Traning Traning { get; set; }
+
+        [ForeignKey(nameof(Salary))]    
+        public int SalaryId { get; set; }
+        public virtual Salary Salary { get; set; }
+    }
+}
