d9e7d940d73199d1fb6b524a004de3f29af12bdb
Akai <nguyensieu12112002@gmail.com>
2024-07-22 21:48:22 +0700

    Create IPerformanceServices.cs

---
+++ HRM.Services.API/performance/IPerformanceServices.cs
@@ -0,0 +1,20 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.performance
+{
+    public interface IPerformanceServices
+    {
+        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformance();
+        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformanceByText(string text);
+        public Task<ServiceResult<Performance>> GetById(int id);
+        public Task<ServiceResult<Performance>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Performance Performance);
+        public Task<ServiceResult<bool>> Update(int id, Performance Performance);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
