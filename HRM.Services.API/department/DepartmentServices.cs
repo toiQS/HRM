9168abb2d2010b74b16bcdfa0be1a85ce986685c
Akai <nguyensieu12112002@gmail.com>
2024-07-22 13:42:22 +0700

    Update DepartmentServices.cs

--- HRM.Services.API/department/DepartmentServices.cs
+++ HRM.Services.API/department/DepartmentServices.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HRM.Services.API.Departments
+namespace HRM.Services.API.department
 {
     public class DepartmentServices
     {
