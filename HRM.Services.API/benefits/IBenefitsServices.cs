ac27a129694f319c9b2d0e30e44b5f5f79f69982
Akai <nguyensieu12112002@gmail.com>
2024-07-22 13:35:44 +0700

    Create IBenefitsServices.cs

---
+++ HRM.Services.API/benefits/IBenefitsServices.cs
@@ -0,0 +1,20 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.benefits
+{
+    public interface IBenefitsServices
+    {
+        public Task<ServiceResult<IEnumerable<Benefits>>> GetBenefits();
+        public Task<ServiceResult<IEnumerable<Benefits>>> GetBenefitsByText(string text);
+        public Task<ServiceResult<Benefits>> GetById(int id);
+        public Task<ServiceResult<Benefits>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Benefits benefits);
+        public Task<ServiceResult<bool>> Update(int id, Benefits benefits);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
