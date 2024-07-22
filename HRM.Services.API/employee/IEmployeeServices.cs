02471c0efd40e86e71af8abda0d9c7078d1b3ca2
Akai <nguyensieu12112002@gmail.com>
2024-07-22 15:57:16 +0700

    Update IEmployeeServices.cs

--- HRM.Services.API/employee/IEmployeeServices.cs
+++ HRM.Services.API/employee/IEmployeeServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace HRM.Services.API.employee
 {
-    internal interface IEmployeeServices
+    public interface IEmployeeServices
     {
+        public Task<ServiceResult<IEnumerable<Employee>>> GetEmployee();
+        public Task<ServiceResult<IEnumerable<Employee>>> GetEmployeeByText(string text);
+        public Task<ServiceResult<Employee>> GetById(int id);
+        public Task<ServiceResult<Employee>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Employee Employee);
+        public Task<ServiceResult<bool>> Update(int id, Employee Employee);
+        public Task<ServiceResult<bool>> Delete(int id);
     }
 }
