6b2a3cb6c9d6f24719915f8e8460a2d93f80c167
Akai <nguyensieu12112002@gmail.com>
2024-07-22 21:48:24 +0700

    Create IEmployeeServices.cs

---
+++ HRM.Services.API/employee/IEmployeeServices.cs
@@ -0,0 +1,20 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.employee
+{
+    public interface IEmployeeServices
+    {
+        public Task<ServiceResult<IEnumerable<Employee>>> GetEmployees();
+        public Task<ServiceResult<IEnumerable<Employee>>> GetEmployeesByText(string text);
+        public Task<ServiceResult<Employee>> GetById(int id);
+        public Task<ServiceResult<Employee>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Employee Employee);
+        public Task<ServiceResult<bool>> Update(int id, Employee Employee);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
