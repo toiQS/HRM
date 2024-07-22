d0d4421934f4812fc6fbf74659ea7a6c2292bd2d
Akai <nguyensieu12112002@gmail.com>
2024-07-22 21:48:28 +0700

    Create IDepartmentServices.cs

---
+++ HRM.Services.API/department/IDepartmentServices.cs
@@ -0,0 +1,20 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.department
+{
+    public interface IDepartmentServices
+    {
+        public Task<ServiceResult<IEnumerable<Department>>> GetDepartments();
+        public Task<ServiceResult<IEnumerable<Department>>> GetDepartmentsByText(string text);
+        public Task<ServiceResult<Department>> GetById(int id);
+        public Task<ServiceResult<Department>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Department Department);
+        public Task<ServiceResult<bool>> Update(int id, Department Department);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
