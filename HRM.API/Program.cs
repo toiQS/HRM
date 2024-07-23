b47476b8b6b47a82189e6b18b821feba9886ff1e
Akai <nguyensieu12112002@gmail.com>
2024-07-23 11:27:27 +0700

    Update Program.cs

--- HRM.API/Program.cs
+++ HRM.API/Program.cs
@@ -1,3 +1,13 @@
+using HRM.Services.API.benefits;
+using HRM.Services.API.department;
+using HRM.Services.API.employee;
+using HRM.Services.API.performance;
+using HRM.Services.API.position;
+using HRM.Services.API.recruitment;
+using HRM.Services.API.salary;
+using HRM.Services.API.shift;
+using HRM.Services.API.training;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBenefitsServices,BenefitsServices>();
+builder.Services.AddScoped<IDepartmentServices,DepartmentServices>();
+builder.Services.AddScoped<IEmployeeServices,EmployeeServices>();
+builder.Services.AddScoped<IPerformanceServices, PerformanceServices>();
+builder.Services.AddScoped<IPositionServices, PositionServices>();
+builder.Services.AddScoped<IRecruitmentServices, RecruitmentServices>();
+builder.Services.AddScoped<ISalaryServices, SalaryServices>();
+builder.Services.AddScoped<IShiftServices, ShiftServices>();
+builder.Services.AddScoped<ITrainingServices, ITrainingServices>();
 
 var app = builder.Build();
 
