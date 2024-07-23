b3df0bdb34549204b30378014fcee153c54a99a2
Akai <nguyensieu12112002@gmail.com>
2024-07-23 11:04:22 +0700

    Create ISalaryServices.cs

---
+++ HRM.Services.API/salary/ISalaryServices.cs
@@ -0,0 +1,19 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.salary
+{
+    public interface ISalaryServices
+    {
+        public Task<ServiceResult<IEnumerable<Salary>>> GetSalaries();
+        public Task<ServiceResult<IEnumerable<Salary>>> GetSalariesByDateRange(DateTime startDate, DateTime endDate);
+        public Task<ServiceResult<Salary>> GetById(int id);
+        public Task<ServiceResult<bool>> Add(Salary salary);
+        public Task<ServiceResult<bool>> Update(int id, Salary salary);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
