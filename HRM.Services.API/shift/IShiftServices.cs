159f7a53a9ebcfc759d8dc82451f19d3d33974c1
Akai <nguyensieu12112002@gmail.com>
2024-07-23 11:04:26 +0700

    Create IShiftServices.cs

---
+++ HRM.Services.API/shift/IShiftServices.cs
@@ -0,0 +1,19 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.shift
+{
+    public interface IShiftServices
+    {
+        public Task<ServiceResult<IEnumerable<Shift>>> GetShifts();
+        public Task<ServiceResult<IEnumerable<Shift>>> GetShiftsByDateRange(DateTime startDate, DateTime endDate);
+        public Task<ServiceResult<Shift>> GetById(int id);
+        public Task<ServiceResult<bool>> Add(Shift shift);
+        public Task<ServiceResult<bool>> Update(int id, Shift shift);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
