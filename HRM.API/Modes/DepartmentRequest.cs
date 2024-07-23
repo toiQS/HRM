90bf08e910e8ad66975f90ae76d5c617bf67024c
Akai <nguyensieu12112002@gmail.com>
2024-07-24 00:54:32 +0700

    Create DepartmentRequest.cs

---
+++ HRM.API/Modes/DepartmentRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace HRM.API.Modes
+{
+    public class DepartmentRequest
+    {
+        public string DepartmentName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
