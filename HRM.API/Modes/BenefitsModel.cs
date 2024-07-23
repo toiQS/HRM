a6fae2a8aa931bb983a6b40820bbe16bfef07630
Akai <nguyensieu12112002@gmail.com>
2024-07-24 00:35:25 +0700

    Create BenefitsModel.cs

---
+++ HRM.API/Modes/BenefitsModel.cs
@@ -0,0 +1,28 @@
+﻿using HRM.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRM.API.Modes
+{
+    public class BenefitsModel
+    {
+    }
+    public class BenefitsResponse
+    {
+        public int Id { get; set; }
+        public string BenefitsName { get; set; } = string.Empty;
+        public string BenefitsDescription { get; set; } = string.Empty;
+        public string TypeBenefits { get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public int PositionId { get; set; }
+    }
+    public class BenefitsRequest
+    {
+        public string BenefitsName { get; set; } = string.Empty;
+        public string BenefitsDescription { get; set; } = string.Empty;
+        public string TypeBenefits { get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public int PositionId { get; set; }
+    }
+}
