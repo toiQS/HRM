013688a75335dfa326237666a30ffe4812622994
Akai <nguyensieu12112002@gmail.com>
2024-07-23 09:20:06 +0700

    Create IRecruitmentServices.cs

---
+++ HRM.Services.API/recruitment/IRecruitmentServices.cs
@@ -0,0 +1,20 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.recruitment
+{
+    public interface IRecruitmentServices
+    {
+        public Task<ServiceResult<IEnumerable<Recruitment>>> GetRecruitments();
+        public Task<ServiceResult<IEnumerable<Recruitment>>> GetRecruitmentsByText(string text);
+        public Task<ServiceResult<Recruitment>> GetById(int id);
+        public Task<ServiceResult<Recruitment>> GetByText(string text);
+        public Task<ServiceResult<bool>> Add(Recruitment recruitment);
+        public Task<ServiceResult<bool>> Update(int id, Recruitment recruitment);
+        public Task<ServiceResult<bool>> Delete(int id);
+    }
+}
