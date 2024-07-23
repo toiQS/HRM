aba8ff597fa17baeb2e3da5f400cd044e895d370
Akai <nguyensieu12112002@gmail.com>
2024-07-23 11:07:12 +0700

    Create ITrainingServices.cs

---
+++ HRM.Services.API/training/ITrainingServices.cs
@@ -0,0 +1,19 @@
+﻿using HRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.training
+{
+    internal interface ITrainingServices
+    {
+        Task<ServiceResult<IEnumerable<Training>>> GetTrainings();
+        Task<ServiceResult<IEnumerable<Training>>> GetTrainingsByDateRange(DateTime startDate, DateTime endDate);
+        Task<ServiceResult<Training>> GetById(int id);
+        Task<ServiceResult<bool>> Add(Training training);
+        Task<ServiceResult<bool>> Update(int id, Training training);
+        Task<ServiceResult<bool>> Delete(int id);
+    }
+}
