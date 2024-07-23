fc8d930333df83158ae344d8cd265b6621edd9f8
Akai <nguyensieu12112002@gmail.com>
2024-07-23 09:19:55 +0700

    Update IPerformanceServices.cs

--- HRM.Services.API/performance/IPerformanceServices.cs
+++ HRM.Services.API/performance/IPerformanceServices.cs
@@ -9,10 +9,10 @@ namespace HRM.Services.API.performance
 {
     public interface IPerformanceServices
     {
-        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformance();
-        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformanceByText(string text);
+        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformances();
+        public Task<ServiceResult<IEnumerable<Performance>>> GetPerformancesByText(string text);
         public Task<ServiceResult<Performance>> GetById(int id);
-        public Task<ServiceResult<Performance>> GetByText(string text);
+        public Task<ServiceResult<Performance>> GetByEvaluationDate(DateTime evaluationDate);
         public Task<ServiceResult<bool>> Add(Performance Performance);
         public Task<ServiceResult<bool>> Update(int id, Performance Performance);
         public Task<ServiceResult<bool>> Delete(int id);
