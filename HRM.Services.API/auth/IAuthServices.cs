a507fccd872af24d11a252f47f1fce4735e23618
Akai <nguyensieu12112002@gmail.com>
2024-07-25 00:08:17 +0700

    Create IAuthServices.cs

---
+++ HRM.Services.API/auth/IAuthServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Services.API.auth
+{
+    public interface IAuthServices
+    {
+        public Task<ServiceResult<bool>> Login(LoginModel loginModel);
+        public Task<ServiceResult<bool>> RegisterUser(RegisterModel registerModel);
+        public Task<ServiceResult<bool>> RegisterAdmin(RegisterModel registerModel);
+        public Task<ServiceResult<bool>> LogOutUser();
+    }
+}
