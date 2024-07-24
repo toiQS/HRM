6bb92213462257d930d82dd372311ba6389852a7
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:04 +0700

    Create ShiftRequest.cs

---
+++ HRM.API/Modes/ShiftRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HRM.API.Modes
+{
+    public class ShiftRequest
+    {
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public List<string> DayOfApplication { get; set; } = new List<string>();
+        public TimeOnly TotalHours { get; set; }
+    }
+}
