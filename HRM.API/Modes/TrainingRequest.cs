4f48019f7fa249e4549e6807e48b43997a371f3c
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:01 +0700

    Create TrainingRequest.cs

---
+++ HRM.API/Modes/TrainingRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace HRM.API.Modes
+{
+    public class TrainingRequest
+    {
+        public string TrainName { get; set; } = string.Empty;
+        public string TrainDescription { get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public List<string> TypeTrain { get; set; } = new List<string>();
+        public string Status { get; set; } = string.Empty;
+    }
+}
