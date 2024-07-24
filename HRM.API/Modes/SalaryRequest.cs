0ecdfe28fb841e240a0014b26e3bf8613fe117d3
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:06 +0700

    Create SalaryRequest.cs

---
+++ HRM.API/Modes/SalaryRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace HRM.API.Modes
+{
+    public class SalaryRequest
+    {
+        public float BasicSalary { get; set; }
+        public float Bonus { get; set; }
+        public float Deductions { get; set; }
+        public float NetSalary { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
