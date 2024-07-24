89288c85d1591151f6394713b77a16c1b91fa725
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:13 +0700

    Create PerformanceRequest.cs

---
+++ HRM.API/Modes/PerformanceRequest.cs
@@ -0,0 +1,15 @@
+﻿using HRM.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRM.API.Modes
+{
+    public class PerformanceRequest
+    {
+        public DateTime EvaluationDate { get; set; }
+        public int Score { get; set; }
+        public string Comment { get; set; } = string.Empty;    
+        public int EmployeeId { get; set; }
+        public int TrainingId { get; set; }
+        public int SalaryId { get; set; }
+    }
+}
