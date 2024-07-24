2b2b51603110c95dfc704b83a221f3efcb4a35be
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:08 +0700

    Create RecruitmentRequest.cs

---
+++ HRM.API/Modes/RecruitmentRequest.cs
@@ -0,0 +1,15 @@
+﻿using HRM.Models;
+
+namespace HRM.API.Modes
+{
+    public class RecruitmentRequest
+    {
+        public string RecruitmentPosition { get; set; } = string.Empty;
+        public DateTime PostingDate { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public List<string> Requestment { get; set; } = new List<string>();
+        public string RecruitmentDescription { get; set; } = string.Empty;
+        public int DepartmentId { get; set; }
+    }
+}
