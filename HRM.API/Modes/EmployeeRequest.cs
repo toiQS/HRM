2d91960050f9d1b610e34d50a3cb4d146bb7c187
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:15 +0700

    Create EmployeeRequest.cs

---
+++ HRM.API/Modes/EmployeeRequest.cs
@@ -0,0 +1,18 @@
+﻿using HRM.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRM.API.Modes
+{
+    public class EmployeeRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+        public int DepartmentId { get; set; }
+        public int PositionId { get; set; }
+        public int ShiftId { get; set; }
+    }
+}
