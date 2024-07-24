13925c8e77737e68bfc689c4d7dfb515166c5071
Akai <nguyensieu12112002@gmail.com>
2024-07-25 00:08:13 +0700

    Create AuthModel.cs

---
+++ HRM.Services.API/auth/AuthModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRM.Services.API.auth
+{
+    public class AuthModel
+    {
+    }
+    public class LoginModel
+    {
+        [EmailAddress]
+        public string Email {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+    public class RegisterModel
+    {
+        public string UserName { get; set; } = string.Empty;
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; 
+    }
+}
