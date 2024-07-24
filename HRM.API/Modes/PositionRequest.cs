dc31fcec33e5fddcdfaccd81563fbdf2aa80d222
Akai <nguyensieu12112002@gmail.com>
2024-07-24 14:49:11 +0700

    Create PositionRequest.cs

---
+++ HRM.API/Modes/PositionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace HRM.API.Modes
+{
+    public class PositionRequest
+    {
+        public string PositionName { get; set; } = string.Empty;
+        public string PositionDescription { get; set; } = string.Empty;
+    }
+}
