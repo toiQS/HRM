4540ea99f885bf25e72e56faab864018966e4228
Akai <nguyensieu12112002@gmail.com>
2024-08-12 14:22:41 +0700

    Create ViewModelBase.cs

---
+++ HRM.WPF/Utilities/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HRM.WPF.Utilities
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void onPropertyChange([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
